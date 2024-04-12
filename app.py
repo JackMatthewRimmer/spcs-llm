249b8089b2c3cac0f2a5ff6c1d69b279f89a92a1
JackMatthewRimmer <jackrimmerwork@outlook.com>
2024-04-12 18:45:06 +0100

    backup

---
+++ app.py
@@ -0,0 +1,96 @@
+from flask import Flask
+from pydantic import BaseModel, model_validator, ValidationError
+from typing import List, Dict, Any
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import os
+
+SERVICE_HOST = os.getenv('SERVICE_HOST', '0.0.0.0')
+SERVICE_PORT = os.getenv('SERVICE_PORT', '8080')
+app = Flask(__name__)
+
+@app.get('/healthcheck')
+def healthcheck():
+    return 'STATUS: READY'
+
+'''
+This endpoint is our entry point for generating a chat completion.
+We are expecting this incoming JSON to be in the format:
+{
+    "data": {
+        [
+            [0, "systemPrompt", "userPrompt", {"inputValues": "someValue"}, {"metadata": "someMetadata"}],
+        ]
+    }
+} 
+'''
+@app.post('/complete')
+def complete():
+    try:
+        request: CompletionRequest = CompletionRequest.model_validate_json(request.json) 
+    except ValidationError as e:
+        return f"Serialization failed with error: {e}"
+    print(f"Request: {request}")
+
+    print("Loading model...")
+    model, tokenizer = load_model()
+    print("Model loaded")
+
+    prompt: str = f'''
+    <s>[INST] <<SYS>>
+    { request.systemPrompt }
+    <</SYS>>
+
+    { request.userPrompt } [/INST]
+    '''
+
+    print(f"Generating chat completion for prompt: {prompt}")
+    print("Encoding prompt...")
+    encoded = tokenizer.encode(prompt, return_tensors='pt')
+    print("Prompt encoded")
+    print("Generating chat completion...")
+    output = model.generate(encoded)
+    print("Chat completion generated")
+    print("Decoding chat completion...")
+    decoded = tokenizer.decode(output[0], skip_special_tokens=True)
+    print(f"Generated chat completion: {decoded}")
+
+    return {
+        "data": {
+           [
+               [0, decoded]
+           ] 
+        }
+    }
+
+
+def load_model():    
+    model = AutoModelForCausalLM.from_pretrained('models/')
+    tokenizer = AutoTokenizer.from_pretrained('models/')
+    model, tokenizer
+
+class CompletionRequest(BaseModel):
+    systemPrompt: str
+    userPrompt: str
+    inputValues: Dict[str, Any]
+    metadata: Dict[str, Any]
+
+    @model_validator(mode='before')
+    def array_to_object(cls, values):
+
+        data_array: List[Any] = values["data"]
+
+        if data_array is None or len(data_array) == 0:
+            return ValidationError(f"data array must contain exactly one row: {values}")
+
+        if len(data_array) > 1:
+            return ValidationError(f"More than one row provided in request {values}")
+
+        return {
+            "systemPrompt": data_array[0][1],
+            "userPrompt": data_array[0][2],
+            "inputValues": data_array[0][3],
+            "metadata": data_array[0][4],
+        }
+
+if __name__ == '__main__':
+    app.run(SERVICE_HOST, SERVICE_PORT, debug=True)
